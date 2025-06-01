42eee4e1e39739164687ef89a3fec97eadbc509c
SoufianeOuenjli <soufianounjli@gmail.com>
2025-06-02 00:32:50 +0100

    refactor(footer, navbar): clean up footer content and update navbar logo link to use route helper

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,53 +1,5 @@
 <!-- Footer Section start  -->
     <footer class="footer-section footer-bg">
-        <div class="container">
-            <div class="contact-info-area">
-                <div class="contact-info-items wow fadeInUp" data-wow-delay=".2s">
-                    <div class="icon">
-                        <i class="icon-icon-5"></i>
-                    </div>
-                    <div class="content">
-                        <p>Appelez-nous 7j/7 et 24h/24</p>
-                        <h3>
-                            <a href="tel:+2085550112">+212-123-4567</a>
-                        </h3>
-                    </div>
-                </div>
-                <div class="contact-info-items wow fadeInUp" data-wow-delay=".4s">
-                    <div class="icon">
-                        <i class="icon-icon-6"></i>
-                    </div>
-                    <div class="content">
-                        <p>Demander un devis</p>
-                        <h3>
-                            <a href="mailto:example@gmail.com">libmanage@gmail.com</a>
-                        </h3>
-                    </div>
-                </div>
-                <div class="contact-info-items wow fadeInUp" data-wow-delay=".6s">
-                    <div class="icon">
-                        <i class="icon-icon-7"></i>
-                    </div>
-                    <div class="content">
-                        <p>Heures d'ouverture</p>
-                        <h3>
-                            Dimanche - Ven : 8h - 18h
-                        </h3>
-                    </div>
-                </div>
-                <div class="contact-info-items wow fadeInUp" data-wow-delay=".8s">
-                    <div class="icon">
-                        <i class="icon-icon-8"></i>
-                    </div>
-                    <div class="content">
-                        <p>Localisation</p>
-                        <h3>
-                            zone fesshore ifmotica.
-                        </h3>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="footer-widgets-wrapper">
             <div class="plane-shape float-bob-y">
                 <img src="assets/img/plane-shape.png" alt="img">
@@ -67,10 +19,10 @@
                                     lacinia mollis
                                 </p>
                                 <div class="social-icon d-flex align-items-center">
-                                    <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
-                                    <a href="https://x.com/"><i class="fab fa-twitter"></i></a>
-                                    <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
-                                    <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                    <a href="#"><i class="fab fa-youtube"></i></a>
+                                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -82,79 +34,18 @@
                             </div>
                             <ul class="list-area">
                                 <li>
-                                    <a href="shop.html">
+                                    <a href="{{ route('shop-default') }}">
                                         <i class="fa-solid fa-chevrons-right"></i>
-                                        Liste des magasins
+                                        Liste des produits
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="contact.html">
+                                    <a href="{{ route('about') }}">
                                         <i class="fa-solid fa-chevrons-right"></i>
-                                        Heures d'ouverture
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="contact.html">
-                                        <i class="fa-solid fa-chevrons-right"></i>
-                                        Contactez-nous
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="contact.html">
-                                        <i class="fa-solid fa-chevrons-right"></i>
-                                        Politique de retour
+                                        À propos de nous
                                     </a>
                                 </li>
                             </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-6 ps-lg-5 wow fadeInUp" data-wow-delay=".6s">
-                        <div class="single-footer-widget">
-                            <div class="widget-head">
-                                <h3>Catégories</h3>
-                            </div>
-                            <ul class="list-area">
-                                <li>
-                                    <a href="shop.html">
-                                        <i class="fa-solid fa-chevrons-right"></i>
-                                        Romans
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="shop.html">
-                                        <i class="fa-solid fa-chevrons-right"></i>
-                                        Livres de poésie
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="contact.html">
-                                        <i class="fa-solid fa-chevrons-right"></i>
-                                        Livres politiques
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="contact.html">
-                                        <i class="fa-solid fa-chevrons-right"></i>
-                                        Livres d'histoire
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay=".8s">
-                        <div class="single-footer-widget">
-                            <div class="widget-head">
-                                <h3>Newsletter</h3>
-                            </div>
-                            <div class="footer-content">
-                                <p>Inscrivez-vous à notre newsletter hebdomadaire pour recevoir les dernières mises à jour.</p>
-                                <div class="footer-input">
-                                    <input type="email" id="email2" placeholder="Entrez votre adresse e-mail">
-                                    <button class="newsletter-btn" type="submit">
-                                        <i class="fa-regular fa-paper-plane"></i>
-                                    </button>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -166,28 +57,6 @@
                     <p class="wow fadeInLeft" data-wow-delay=".3s">
                         © Tous droits réservés 2025 par <a href="index.html">LibManage</a>
                     </p>
-                    <ul class="brand-logo wow fadeInRight" data-wow-delay=".5s">
-                        <li>
-                            <a href="contact.html">
-                                <img src="assets/img/visa-logo.png" alt="img">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="contact.html">
-                                <img src="assets/img/mastercard.png" alt="img">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="contact.html">
-                                <img src="assets/img/payoneer.png" alt="img">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="contact.html">
-                                <img src="assets/img/affirm.png" alt="img">
-                            </a>
-                        </li>
-                    </ul>
                 </div>
             </div>
         </div>
