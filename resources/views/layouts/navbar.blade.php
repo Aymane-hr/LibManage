00b994bd741987e23044c6a525dc398aedd36c7c
SoufianeOuenjli <soufianounjli@gmail.com>
2025-06-02 00:58:13 +0100

    style(header): adjust header dimensions and margins for improved layout

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,20 +1,20 @@
 <!-- Sticky Header Section start  -->
-<header class="header-1 sticky-header">
-    <div class="mega-menu-wrapper">
-        <div class="header-main">
-            <div class="container">
-                <div class="row">
-                    <div class="col-6 col-md-6 col-lg-10 col-xl-8 col-xxl-10">
-                        <div class="header-left">
-                            <div class="logo">
-                                <a href="{{ route('home') }}" class="header-logo">
+<header class="header-1 sticky-header" style="transform: translateY(0px);">
+        <div class="mega-menu-wrapper">
+            <div class="header-main">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-6 col-md-6 col-lg-10 col-xl-8 col-xxl-10">
+                            <div class="header-left">
+                                <div class="logo">
+                                    <a href="{{ route('home') }}" class="header-logo">
                                     <img src="assets/img/logo/logo.png" alt="logo-img">
                                 </a>
-                            </div>
-                            <div class="mean__menu-wrapper">
-                                <div class="main-menu" style="margin-left: 100px;">
-                                    <nav>
-                                        <ul>
+                                </div>
+                                <div class="mean__menu-wrapper">
+                                    <div class="main-menu">
+                                        <nav>
+                                            <ul>
                                             {{-- hadi ghi khlih --}}
                                             <li class="has-dropdown">
                                                 <a href="{{ route('home') }}">
@@ -43,44 +43,15 @@
                                                 </li>
                                             @endguest
                                         </ul>
-                                    </nav>
+                                        </nav>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-6 col-md-6 col-lg-2 col-xl-4 col-xxl-2">
-                        <div class="header-right">
-                            {{-- <div class="category-oneadjust gap-6 d-flex align-items-center">
-                                <div class="icon">
-                                    <i class="fa-sharp fa-solid fa-grid-2"></i>
-                                </div>
-                                <select name="cate" class="category">
-                                    <option value="1">
-                                        Catégorie
-                                    </option>
-                                    <option value="1">
-                                        Web Design
-                                    </option>
-                                    <option value="1">
-                                        Développement Web
-                                    </option>
-                                    <option value="1">
-                                        Design Graphique
-                                    </option>
-                                    <option value="1">
-                                        Ingénierie Logicielle
-                                    </option>
-                                </select>
-                                <form action="#" class="search-toggle-box d-md-block">
-                                    <div class="input-area">
-                                        <input type="text" placeholder="Auteur">
-                                        <button class="cmn-btn">
-                                            <i class="far fa-search"></i>
-                                        </button>
-                                    </div>
-                                </form>
-                            </div> --}}
-                            <div class="menu-cart">
+                        <div class="col-6 col-md-6 col-lg-2 col-xl-4 col-xxl-2">
+                            <div class="header-right">
+
+                                <div class="menu-cart">
                                 @auth
                                     <a href="{{route('favoris')}}" class="cart-icon">
                                         <i class="fa-regular fa-heart"></i>
@@ -106,34 +77,33 @@
                                     </a>
                                 </div> --}}
                             </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+
+
             </div>
-
-
-
         </div>
-    </div>
-</header>
+    </header>
 
 <!-- Main Header Section start  -->
 <header class="header-1">
-    <div class="mega-menu-wrapper">
-        <div class="header-main">
-            <div class="container">
-                <div class="row">
-                    <div class="col-6 col-md-6 col-lg-10 col-xl-8 col-xxl-10">
-                        <div class="header-left">
-                            <div class="logo" style="margin-bottom: 68px;">
-                                <a href="index.html" class="header-logo">
-                                    <img src="assets/img/logo/logo.png" alt="logo-img">
-                                </a>
-                            </div>
-                            <div class="mean__menu-wrapper">
-                                <div class="main-menu" style="margin-left: 100px;">
-                                    <nav>
-                                        <ul>
+        <div class="mega-menu-wrapper">
+            <div class="header-main">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-6 col-md-6 col-lg-10 col-xl-8 col-xxl-10">
+                            <div class="header-left">
+                                <div class="logo">
+                                    <a href="index.html" class="header-logo">
+                                        <img src="assets/img/logo/logo.png" alt="logo-img">
+                                    </a>
+                                </div>
+                                <div class="mean__menu-wrapper">
+                                    <div class="main-menu">
+                                        <nav id="mobile-menu" style="display: block;">
+                                            <ul>
                                             {{-- hadi ghi khlih --}}
                                             <li class="has-dropdown">
                                                 <a href="{{ route('home') }}">
@@ -162,44 +132,15 @@
                                                 </li>
                                             @endguest
                                         </ul>
-                                    </nav>
+                                        </nav>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-6 col-md-6 col-lg-2 col-xl-4 col-xxl-2">
-                        <div class="header-right">
-                            {{-- <div class="category-oneadjust gap-6 d-flex align-items-center">
-                                <div class="icon">
-                                    <i class="fa-sharp fa-solid fa-grid-2"></i>
-                                </div>
-                                <select name="cate" class="category">
-                                    <option value="1">
-                                        Catégorie
-                                    </option>
-                                    <option value="1">
-                                        Web Design
-                                    </option>
-                                    <option value="1">
-                                        Développement Web
-                                    </option>
-                                    <option value="1">
-                                        Design Graphique
-                                    </option>
-                                    <option value="1">
-                                        Ingénierie Logicielle
-                                    </option>
-                                </select>
-                                <form action="#" class="search-toggle-box d-md-block">
-                                    <div class="input-area">
-                                        <input type="text" placeholder="Auteur">
-                                        <button class="cmn-btn">
-                                            <i class="far fa-search"></i>
-                                        </button>
-                                    </div>
-                                </form>
-                            </div> --}}
-                            <div class="menu-cart">
+                        <div class="col-6 col-md-6 col-lg-2 col-xl-4 col-xxl-2">
+                            <div class="header-right">
+
+                               <div class="menu-cart">
                                 @auth
                                     <a href="{{route('favoris')}}" class="cart-icon">
                                         <i class="fa-regular fa-heart"></i>
@@ -226,7 +167,7 @@
                                     </a>
                                 </div> --}}
                             </div>
-                            <script>
+                                <script>
                                 document.addEventListener('DOMContentLoaded', function() {
                                     fetch('/cart')
                                         .then(response => response.json())
@@ -258,12 +199,12 @@
                                     })
                                     .catch(error => console.error('Erreur lors de la récupération des favoris :', error));
                             </script>
+                            </div>
                         </div>
                     </div>
                 </div>
+
+
             </div>
-
-
         </div>
-    </div>
-</header>
+    </header>
